95a651ff12d62add80eb7d72f236d972cad5750b
Mohamed <elsebaey95@outlook.com>
2025-02-11 01:20:36 +0200

    remove .

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
         >
-          Software Developer.
+          Software Developer
         </motion.h1>
 
         <motion.p
